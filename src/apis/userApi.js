e9377b419476faba751c3e6addb17d965fb4635d
H-JuKyung <hjk000hjk17@gmail.com>
2025-05-23 03:10:01 +0900

    Refactor: VITE_BACK_URL 환경변수에 의존하도록 기본값 제거

--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
 axios.defaults.withCredentials = true;
-const API_URL = import.meta.env.VITE_BACK_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_BACK_URL;
 
 export const registerUser = async userData => {
   const response = await axios.post(`${API_URL}/register`, userData);
